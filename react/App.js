1eb0fa07f1031372e86be44f8670845f73e497df
JQ-KF 01 <JQ-KF 01@PCOS-2001211348>
2020-03-07 18:31:20 +0800

    ..

---
+++ react/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+      var data = [
+        { title: "唱", finished: false },
+        { title: "跳", finished: false },
+        { title: "rap", finished: true }
+      ];  
+
+    comp
+    
+  return (
+    <div>
+      <TodoHead></TodoHead>
+    
+    </div>
+  );
+}
+
+export default App;
